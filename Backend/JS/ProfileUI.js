7cfb20537c8d4138d11410b889f282a214db4a6c
mohit1609 <mohitshah.ict19@gmail.com>
2021-04-15 16:00:14 +0530

    UI updation
    frontend to change profile details added

--- Backend/JS/ProfileUI.js
+++ Backend/JS/ProfileUI.js
@@ -9,3 +9,39 @@ function disp ()
         document.getElementById("1exp").style.display = "block";
     }
 }
+
+function change_email ()
+{
+    if (document.getElementById("email_change").style.display == "block")
+    {
+        document.getElementById("email_change").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("email_change").style.display = "block";
+    }
+}
+
+function change_phone ()
+{
+    if (document.getElementById("mobile_change").style.display == "block")
+    {
+        document.getElementById("mobile_change").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("mobile_change").style.display = "block";
+    }
+}
+
+function change_address ()
+{
+    if (document.getElementById("address_change").style.display == "block")
+    {
+        document.getElementById("address_change").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("address_change").style.display = "block";
+    }
+}
